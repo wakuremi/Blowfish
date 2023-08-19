114f24a237e5311e5a51f530f165c4b9571dee0a
wakuremi <67831008+wakuremi@users.noreply.github.com>
2023-08-19 11:41:25 +0500

    Удалил все из Blowfish.Game

--- Blowfish.Program/Composition/Modules/GameModule.cs
+++ Blowfish.Program/Composition/Modules/GameModule.cs
@@ -1,10 +1,4 @@
 ﻿using Blowfish.Engine.Entities;
-using Blowfish.Engine.Graphics;
-using Blowfish.Game;
-using Blowfish.Game.Creatures;
-using Blowfish.Game.Creatures.Renderers;
-using Blowfish.Game.Creatures.Updaters;
-using Ninject;
 using Ninject.Modules;
 
 namespace Blowfish.Program.Composition.Modules;
@@ -17,77 +11,5 @@ public sealed class GameModule : NinjectModule
 
     public override void Load()
     {
-        _ = Bind<IEntityUpdater>()
-            .To<EntityUpdaterAggregator>()
-            .InSingletonScope()
-            .WithConstructorArgument(
-                "updaters",
-                ctx => new IEntityUpdater[]
-                {
-                    ctx.Kernel.Get<InitializerEntityUpdater>(),
-                    ctx.Kernel.Get<TrackEntityUpdater>(),
-                    ctx.Kernel.Get<UserInputEntityUpdater>(),
-                    ctx.Kernel.Get<MovementEntityUpdater>()
-                }
-                );
-
-        _ = Bind<InitializerEntityUpdater>()
-            .ToSelf()
-            .InSingletonScope();
-
-        _ = Bind<TrackEntityUpdater>()
-            .ToSelf()
-            .InSingletonScope();
-
-        _ = Bind<UserInputEntityUpdater>()
-            .ToSelf()
-            .InSingletonScope();
-
-        _ = Bind<MovementEntityUpdater>()
-            .ToSelf()
-            .InSingletonScope();
-
-        _ = Bind<IEntityRenderer>()
-            .To<EntityRendererAggregator>()
-            .When(r =>
-            {
-                // Чтобы агрегатор не инжектился сам в себя.
-                return r.Target == null
-                    || r.Target.Member.ReflectedType != typeof(EntityRendererAggregator);
-            })
-            .InSingletonScope();
-
-        _ = Bind<IEntityRenderer>()
-            .To<CreatureEntityRenderer>()
-            .WhenInjectedExactlyInto<EntityRendererAggregator>()
-            .InSingletonScope();
-
-        _ = Bind<ICreatureRenderer>()
-            .To<CreatureRendererAggregator>()
-            .When(r =>
-            {
-                // Чтобы агрегатор не инжектился сам в себя.
-                return r.Target == null
-                    || r.Target.Member.ReflectedType != typeof(CreatureRendererAggregator);
-            })
-            .InSingletonScope();
-
-        _ = Bind<ICreatureRenderer>()
-            .To<PlayerCreatureRenderer>()
-            .WhenInjectedExactlyInto<CreatureRendererAggregator>()
-            .InSingletonScope();
-
-        _ = Bind<ICreatureRenderer>()
-            .To<CrateCreatureRenderer>()
-            .WhenInjectedExactlyInto<CreatureRendererAggregator>()
-            .InSingletonScope();
-
-        // TODO: temp
-        _ = Bind<SpriteSheet>()
-            .ToSelf()
-            .InSingletonScope()
-            .WithConstructorArgument("filePath", "Assets/Sprites.png")
-            .WithConstructorArgument("spriteWidth", 16)
-            .WithConstructorArgument("spriteHeight", 16);
     }
 }
